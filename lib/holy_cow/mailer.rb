223d272cffefc9057b0ae548e628cfa4237a1fd2
Brian Samson <brian@briansamson.com>
2014-09-11 20:58:19 -0500

    add text format, bump version

--- lib/holy_cow/mailer.rb
+++ lib/holy_cow/mailer.rb
@@ -1,17 +1,17 @@
 require 'action_mailer'
 require 'pp'
+ActionMailer::Base.append_view_path File.join(File.dirname(__FILE__), "..")
 class HolyCow::Mailer < ActionMailer::Base
   def notification(*args)
     @subject = args.shift || "Somebody did something!"
-    template = File.join(File.dirname(__FILE__), "mailer", "notification.html.erb")
-    body = ERB.new(File.read(template))
     @things = args || []
     mail({
       from: HolyCow::from_address, 
       to: HolyCow::recipients, 
-      subject: "[Holy Cow] #{@subject}", 
-      body: body.result(binding),
-      content_type: "text/html"
-    })
+      subject: "[Holy Cow] #{@subject}"
+    }) do |format|
+      format.html
+      format.text
+    end
   end
 end
